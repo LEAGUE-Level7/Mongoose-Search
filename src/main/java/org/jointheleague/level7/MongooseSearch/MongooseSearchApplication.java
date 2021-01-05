ba68d9262a36a61cd4e483324c43a4222f939a57
mjfre <mjfre@aol.com>
2021-01-04 17:29:00 -0800

    initial

---
+++ src/main/java/org/jointheleague/level7/MongooseSearch/MongooseSearchApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.level7.MongooseSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongooseSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongooseSearchApplication.class, args);
+	}
+
+}
