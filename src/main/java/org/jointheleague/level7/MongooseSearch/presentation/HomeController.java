251b3fd9c6292fccbc81e5b52e98828df3408146
harrisoncliew <41922141+harrisoncliew@users.noreply.github.com>
2021-01-11 18:24:04 -0800

    Redirecting base url to swagger-ui

--- src/main/java/org/jointheleague/level7/MongooseSearch/presentation/HomeController.java
+++ src/main/java/org/jointheleague/level7/MongooseSearch/presentation/HomeController.java
@@ -1,12 +1,15 @@
 package org.jointheleague.level7.MongooseSearch.presentation;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home(){
+    @ResponseStatus(HttpStatus.MOVED_PERMANENTLY)
+    public String home() {
         return "redirect:swagger-ui.html";
     }
 }
