0121f787f720666640486bb110f2e5d0bbd63204
Isaacpants <dave@boyermoving.com>
2021-01-25 18:14:22 -0800

    Adding web client and making request.

--- src/main/java/org/jointheleague/level7/MongooseSearch/presentation/repository/FlightRepository.java
+++ src/main/java/org/jointheleague/level7/MongooseSearch/presentation/repository/FlightRepository.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Repository
 public class FlightRepository {
@@ -20,7 +21,16 @@ public class FlightRepository {
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE) // Content header things
                 .build(); // builds webClient
     }
+    //http://api.aviationstack.com/v1/flights?access_key=747ca55c8b46ed9f3b1956e6b7eb6394&arr_iata=SAN
     public String getArrivingFlights(String iataAirportCode){
-        return "iata code is: " + iataAirportCode;
+        Mono<String> stringMono = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("access_key", "747ca55c8b46ed9f3b1956e6b7eb6394")
+                        .queryParam("&arr_iata", iataAirportCode)
+                        .build()
+                ).retrieve()
+                .bodyToMono(String.class); // allows synchronous request(I think)
+        return stringMono.block(); // waits for the request to complete
+
     }
 }
