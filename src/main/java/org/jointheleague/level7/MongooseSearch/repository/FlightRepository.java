d810426b9a5866604f2b8198ceefa76b8d778dd1
harrisoncliew <41922141+harrisoncliew@users.noreply.github.com>
2021-02-01 18:26:34 -0800

    Returning AviationStackResponse instead of String

--- src/main/java/org/jointheleague/level7/MongooseSearch/repository/FlightRepository.java
+++ src/main/java/org/jointheleague/level7/MongooseSearch/repository/FlightRepository.java
@@ -1,5 +1,6 @@
 package org.jointheleague.level7.MongooseSearch.repository;
 
+import org.jointheleague.level7.MongooseSearch.repository.DTO.AviationStackResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
@@ -20,15 +21,49 @@ public class FlightRepository {
                 .build();
     }
     //http://api.aviationstack.com/v1/flights?access_key=747ca55c8b46ed9f3b1956e6b7eb6394&arr_iata=SAN
-    public String getArrivingFlights(String iataAirportCode){
-        Mono<String> stringMono = webClient
+    public AviationStackResponse getArrivingFlights(String iataAirportCode){
+        Mono<AviationStackResponse> aviationStackResponseMono = webClient
                 .get()
                 .uri(uriBuilder -> uriBuilder
                         .queryParam("access_key", accessKey)
                         .queryParam("arr_iata", iataAirportCode)
-                        .build()
-                ).retrieve()
-                .bodyToMono(String.class);
-        return stringMono.block();
+                        .build())
+                .retrieve()
+                .bodyToMono(AviationStackResponse.class);
+        return aviationStackResponseMono.block();
     }
+
+    /*
+    {
+      "flight_date": "2021-02-01",
+      "flight_status": "landed",
+      "departure": {
+        "airport": "Seattle-Tacoma International",
+        "iata": "SEA",
+      },
+      "arrival": {
+        "airport": "San Diego International Airport",
+        "timezone": "America/Los_Angeles",
+        "iata": "SAN",
+        "terminal": "2",
+        "gate": "20",
+        "scheduled": "2021-02-01T14:00:00+00:00",
+        "estimated": "2021-02-01T14:00:00+00:00",
+        "actual": "2021-02-01T13:56:00+00:00",
+      },
+      "airline": {
+        "name": "Korean Air",
+        "iata": "KE",
+      },
+      "flight": {
+        "number": "6125",
+        "iata": "KE6125",
+        "codeshared": {
+          "airline_name": "alaska airlines",
+          "flight_iata": "as1478",
+        }
+      }
+
+    }
+     */
 }
