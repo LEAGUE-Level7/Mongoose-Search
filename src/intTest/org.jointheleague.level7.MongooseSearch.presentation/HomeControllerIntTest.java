f871fb0f62c321fb856da2a0fcafe910c5bbc0c7
Isaacpants <dave@boyermoving.com>
2021-03-15 18:26:21 -0700

    ip

---
+++ src/intTest/org.jointheleague.level7.MongooseSearch.presentation/HomeControllerIntTest.java
@@ -0,0 +1,20 @@
+package org.jointheleague.level7.MongooseSearch.presentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerIntTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void whenHome_ThenReturnMovedPermanentlyAndRedirect(){
+
+    }
+
+}
+
+
