7c56f01de679f75a32e7ccfee3c61b976972f71f
harrisoncliew <41922141+harrisoncliew@users.noreply.github.com>
2021-03-15 17:36:29 -0700

    Finishing Unit Tests

--- src/test/java/org/jointheleague/level7/MongooseSearch/repository/FlightRepositoryTest.java
+++ src/test/java/org/jointheleague/level7/MongooseSearch/repository/FlightRepositoryTest.java
@@ -6,32 +6,40 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
 import reactor.core.publisher.Mono;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import java.net.URI;
+import java.util.function.Function;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
+
 import static org.mockito.Mockito.when;
 
 class FlightRepositoryTest {
-private FlightRepository flightRepository;
-@Mock
-private WebClient webClient;
-@Mock
-WebClient webClientMock;
-@Mock
-WebClient.RequestHeadersUriSpec requestHeadersUriSpecMock;
-@Mock
-WebClient.RequestHeadersSpec requestHeadersSpecMock;
-@Mock
-WebClient.ResponseSpec responseSpecMock;
-@Mock
-Mono<AviationStackResponse> AviationStackResponseMonoMock;
+    private FlightRepository flightRepository;
+    @Mock
+    WebClient webClientMock;
+
+    @Mock
+    WebClient.RequestHeadersUriSpec requestHeadersUriSpecMock;
+
+    @Mock
+    WebClient.RequestHeadersSpec requestHeadersSpecMock;
+
+    @Mock
+    WebClient.ResponseSpec responseSpecMock;
+
+    @Mock
+    Mono<AviationStackResponse> AviationStackMonoMock;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        flightRepository = new FlightRepository();
-        flightRepository.setWebClient(webClient);
+        flightRepository = new FlightRepository(webClientMock);
+
     }
 
     @Test
@@ -41,14 +49,14 @@ Mono<AviationStackResponse> AviationStackResponseMonoMock;
         AviationStackResponse expectedAviationStackResponse = new AviationStackResponse();
         when(webClientMock.get())
                 .thenReturn(requestHeadersUriSpecMock);
-        when(requestHeadersUriSpecMock.uri(anyString()))
+        when(requestHeadersUriSpecMock.uri((Function<UriBuilder, URI>) any()))
                 .thenReturn(requestHeadersSpecMock);
         when(requestHeadersSpecMock.retrieve())
                 .thenReturn(responseSpecMock);
         when(responseSpecMock.bodyToMono(AviationStackResponse.class))
-                .thenReturn(AviationStackResponseMonoMock);
-        when(AviationStackResponseMonoMock.block())
-                .thenReturn(AviationStackResponse);;
+                .thenReturn(AviationStackMonoMock);
+        when(AviationStackMonoMock.block())
+                .thenReturn(expectedAviationStackResponse);
         //when
         AviationStackResponse actualAviationStackResponse = flightRepository.getArrivingFlights(iataAirportCode);
         //then
@@ -56,6 +64,23 @@ Mono<AviationStackResponse> AviationStackResponseMonoMock;
     }
 
     @Test
-    void getDepartingFlights() {
+    void whenGetDepartingFlights_thenReturnAviationStackResponse() {
+        //given
+        String iataAirportCode = "SAN";
+        AviationStackResponse expectedAviationStackResponse = new AviationStackResponse();
+        when(webClientMock.get())
+                .thenReturn(requestHeadersUriSpecMock);
+        when(requestHeadersUriSpecMock.uri((Function<UriBuilder, URI>) any()))
+                .thenReturn(requestHeadersSpecMock);
+        when(requestHeadersSpecMock.retrieve())
+                .thenReturn(responseSpecMock);
+        when(responseSpecMock.bodyToMono(AviationStackResponse.class))
+                .thenReturn(AviationStackMonoMock);
+        when(AviationStackMonoMock.block())
+                .thenReturn(expectedAviationStackResponse);
+        //when
+        AviationStackResponse actualAviationStackResponse = flightRepository.getDepartingFlights(iataAirportCode);
+        //then
+        assertEquals(expectedAviationStackResponse, actualAviationStackResponse);
     }
 }
