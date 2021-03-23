2b8c13f5a4af70d89b12a69fd7bcb9bc0eb294b6
Isaacpants <dave@boyermoving.com>
2021-03-22 17:48:51 -0700

    Finishing one integration test

--- src/intTest/java/org/jointheleague/level7/MongooseSearch/presentation/HomeControllerIntTest.java
+++ src/intTest/java/org/jointheleague/level7/MongooseSearch/presentation/HomeControllerIntTest.java
@@ -5,16 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 @WebMvcTest(HomeController.class)
 public class HomeControllerIntTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void whenHome_ThenReturnMovedPermanentlyAndRedirect(){
-
+    public void whenHome_ThenReturnMovedPermanentlyAndRedirect() throws Exception {
+    //try catch for important code that is needed to make code do stuff
+    mockMvc.perform(get("/")) //makes the request --> request URI = "/"
+    .andDo(print())
+    .andExpect(status().isMovedPermanently())
+    .andExpect(redirectedUrl("swagger-ui.html"));
     }
 
 }
-
-
